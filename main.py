ad959dace8373c931143ebba31ffa22c78f29242
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2024-03-18 12:04:46 +0000

    Added custom prompts

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
 import toml
+from rich import print
+from rich.prompt import Prompt
 import sys
 
-global_config = {
-    "config_path": "prompt.config.toml",
-    "help": False
-}
+global_config = {"config_path": "prompt.config.toml", "help": False}
+
+
+def rich_input(input_message: str):
+	print(input_message, end="")
+	input("")
+
+
+def display_help(help_dict: dict):
+	pass
+
 
 def parse_prompt():
-    args = sys.argv[:1]
-    flags = {
-        "-c": ("config_path", False),
-        "-h": ("help", True)
-    }
+	args = sys.argv[1:]
+	flags = {"-c": ("config_path", False), "-h": ("help", True)}
 
-    for flag, global_config_data in flags.items():
-        global_config_bool = global_config_data[0]
-        global_config_flag = global_config_data[1]
+	for flag, flag_data in flags.items():
+		flag_name = flag_data[0]
+		flag_bool = flag_data[1]
+		if flag not in args:
+			continue
 
-        if flag not in args:
-            continue
-        
-        try:
-            flag_index = args.index(flag)
-            if global_config_bool:
-                flags[flag] = not (flags[flag])
-            else:
-                flags[flag] = args[flag_index + 1]
-        except IndexError:
-            print("Incorrect flags supplied")
-            quit()
+		try:
+			flag_index = args.index(flag)
+			if flag_bool:
+				global_config[flag_name] = not (global_config[flag_name])
+			else:
+				global_config[flag_name] = args[flag_index + 1]
+		except IndexError:
+			quit()
 
-parse_prompt()            
+
+
+
+parse_prompt()
 
 if global_config["help"]:
-    print("Help stuff!")
-    quit()
+	print("Help stuff!")
+	quit()
 config = toml.load(global_config["config_path"])
+prompt_config = config["prompt"]
+message_config = prompt_config["message"]
 
-
+prompt = f"[{message_config['colour']}]{message_config['text']}[/{message_config['colour']}]"
+rich_input(prompt)
+# Prompt.ask(prompt)
