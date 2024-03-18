8410032de2fd713f3feb625c3780579bd8b9bcb7
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2024-03-18 09:19:12 +0000

    Added help flag

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-from _typeshed import IndexableBuffer
 import toml
 import sys
 
 global_config = {
-    "config_path": "prompt.config.toml"
+    "config_path": "prompt.config.toml",
+    "help": False
 }
 
 def parse_prompt():
     args = sys.argv[:1]
     flags = {
-        "-c": "config_path"
+        "-c": ("config_path", False),
+        "-h": ("help", True)
     }
 
-    for flag, global_config_key in flags.items():
+    for flag, global_config_data in flags.items():
+        global_config_bool = global_config_data[0]
+        global_config_flag = global_config_data[1]
+
         if flag not in args:
             continue
         
         try:
             flag_index = args.index(flag)
-            flags[flag] = args[flag_index + 1]
+            if global_config_bool:
+                flags[flag] = not (flags[flag])
+            else:
+                flags[flag] = args[flag_index + 1]
         except IndexError:
             print("Incorrect flags supplied")
             quit()
 
 parse_prompt()            
 
+if global_config["help"]:
+    print("Help stuff!")
+    quit()
 config = toml.load(global_config["config_path"])
 
 
