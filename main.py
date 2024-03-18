06c1aeb37e4106803c7f8bf23f6d59d06065cea8
Nathan <117750247+Nathan3-14@users.noreply.github.com>
2024-03-18 13:33:51 +0000

    Added multi-colour prompts

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import toml
 from rich import print
-from rich.prompt import Prompt
 import sys
 
 global_config = {"config_path": "prompt.config.toml", "help": False}
@@ -8,7 +7,7 @@ global_config = {"config_path": "prompt.config.toml", "help": False}
 
 def rich_input(input_message: str):
 	print(input_message, end="")
-	input("")
+	return input("")
 
 
 def display_help(help_dict: dict):
@@ -44,8 +43,11 @@ if global_config["help"]:
 	quit()
 config = toml.load(global_config["config_path"])
 prompt_config = config["prompt"]
-message_config = prompt_config["message"]
+message_config = prompt_config["messages"]
 
-prompt = f"[{message_config['colour']}]{message_config['text']}[/{message_config['colour']}]"
-rich_input(prompt)
-# Prompt.ask(prompt)
+prompt = ""
+for text, text_data in message_config.items():
+	prompt += f"[{text_data['colour']}]{text_data['text']}[/{text_data['colour']}]"
+
+while True:
+  print(rich_input(prompt))
